b587da611a8e143fc25473d6dbd893e1260ad246
jdsamar <samarjeet360@gmail.com>
2022-01-18 01:38:20 +0530

    nodeapp final commit

--- assignments/assignment_3/app.js
+++ assignments/assignment_3/app.js
@@ -1,29 +1,53 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+const req = require("express/lib/request");
 const app = express();
-const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:true}));
+app.use(bodyparser());
 
-let users = [];
-
-
-app.get("/", (req, res) => {
-    res.render('mainpage.ejs', {data:users});
-})
-
-app.get('/form', (req, res) => {
-    res.render('form.ejs');
-})
-
-app.post('/user/add', (req, res) => {
-    const userData = {
-        name: req.body.name,
-        email: req.body.email
+var users = [
+    {
+        name: "xyz",
+        email: "xyz@xyz.com"
+    },
+    {
+        name: "admin",
+        email: "admin@admin.com"
+    },
+    {
+        name: "10x Academy",
+        email: "10X@10xacademy.com"
     }
-    users.push(userData)
-    res.render('mainpage.ejs', { data: users});
+];
+
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+
+
+
+app.get("/",(req, res) => {
+    res.render("mainpage.ejs", {users});
 });
+
+
+app.get("/form",(req, res) => {
+    res.render("form.ejs");
+});
+
+
+app.post("/add/user", (req, res) => {
+    users.push({
+        name: req.body.name,
+        email: req.body.email,
+    })
+    res.redirect("/");
+});
+
+
+
 
 app.listen(3000, () => {
     console.log("Server is listening on Port 3000")
